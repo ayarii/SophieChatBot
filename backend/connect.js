e81a5c166d737cc219e1697b7b4f9db471c1e434
tourabio <mohamedsayed.tourabi@esprit.tn>
2021-03-27 23:38:48 +0100

    connexion avec mongoo port 27017 (par defaut)

--- backend/connect.js
+++ backend/connect.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
+const config = require('./database/mongodb.json'); 
 
 class Connexion {
     constructor() {
-        mongoose.connect('mongodb+srv://hr:0000@cluster0.ba3ru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+        mongoose.connect(config.mongo.uri, {useNewUrlParser : true, useUnifiedTopology : true},
+            ()=> console.log('Connected to DB'));
+
+
+
+
+      /*  mongoose.connect('mongodb+srv://hr:0000@cluster0.ba3ru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
             })
             .then(() => console.log('Connexion à MongoDB réussie !'))
             .catch(() => console.log('Connexion à MongoDB échouée !'));
-    }
+        */
+       
+        } 
 }
 module.exports = Connexion
